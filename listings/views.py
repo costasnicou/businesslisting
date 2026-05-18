8d086285b6f41e1c645c3a4186fe2d34575f7b05
Kostas Nikou <nicoucostas@gmail.com>
2026-05-18 07:05:03 +0300

    latest_update

--- listings/views.py
+++ listings/views.py
@@ -37,6 +37,37 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "listings/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "listings/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        cities = City.objects.all()
+        categories =  BusinessCategory.objects.all()
+        return render(request, "listings/register.html",{
+            "cities":cities,
+            "categories":categories
+        })
+
 
 def index(request):
     cities = City.objects.all()
