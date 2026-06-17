8f979385d58bdcb2fbbbfad6724ed0910961d951
Costas Nicou <nicoycostas@gmail.com>
2026-06-17 09:01:48 +0300

    category-scss-change-cover-change

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/cnicou/Desktop/Git Repositories/businesslisting/venv'
+set -gx VIRTUAL_ENV /home/cnicou/Desktop/CS50/businesslisting/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
